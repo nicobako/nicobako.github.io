dc7c9f73020e022b5fb35af426d74a7202638d30
Nico Bako <26750598+nicobako@users.noreply.github.com>
2023-07-23 04:04:40 -0500

    Update sw.js

--- pwa/sw.js
+++ pwa/sw.js
@@ -1,15 +1,30 @@
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open('sw-cache').then(function(cache) {
-      return cache.add('index.html');
+const cacheName = 'nicobako';
+
+// Cache all the files to make a PWA
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      // Our application only has two files here index.html and manifest.json
+      // but you can add more such as style.css as your app grows
+      return cache.addAll([
+        './',
+        './index.html',
+        './manifest.json',
+        './static/style.css',
+      ]);
     })
   );
 });
- 
-self.addEventListener('fetch', function(event) {
+
+// Our service worker will intercept all fetch requests
+// and check if we have cached the file
+// if so it will serve the cached file
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
+    caches.open(cacheName)
+      .then(cache => cache.match(event.request, { ignoreSearch: true }))
+      .then(response => {
+        return response || fetch(event.request);
+      })
   );
 });
