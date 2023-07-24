b2425993b9e50e7bb859fa272026a5785aa8169e
Nico Bako <26750598+nicobako@users.noreply.github.com>
2023-07-24 01:02:20 -0500

    Update sw.js

--- pwa/sw.js
+++ pwa/sw.js
@@ -19,15 +19,27 @@ self.addEventListener("install", (event) => {
   );
 });
 
-// Our service worker will intercept all fetch requests
-// and check if we have cached the file
-// if so it will serve the cached file
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", (event) => {
+  // Let the browser do its default thing
+  // for non-GET requests.
+  if (event.request.method !== "GET") return;
+
+  // Prevent the default, and handle the request ourselves.
   event.respondWith(
-    caches.open(cacheName)
-      .then(cache => cache.match(event.request, { ignoreSearch: true }))
-      .then(response => {
-        return response || fetch(event.request);
-      })
+    (async () => {
+      // Try to get the response from a cache.
+      const cache = await caches.open(cacheName);
+      const cachedResponse = await cache.match(event.request);
+
+      if (cachedResponse) {
+        // If we found a match in the cache, return it, but also
+        // update the entry in the cache in the background.
+        event.waitUntil(cache.add(event.request));
+        return cachedResponse;
+      }
+
+      // If we didn't find a match in the cache, use the network.
+      return fetch(event.request);
+    })(),
   );
 });
