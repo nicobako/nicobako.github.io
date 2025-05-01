4ac7ffc391aba23d237133dabdaec144fc5fc483
nicobako <nicobako@gmail.com>
2025-05-01 08:36:03 -0500

    1000 words added as a resource

--- README.md
+++ README.md
@@ -2,19 +2,7 @@
 
 Personal site.
 
-Serves mostly as a playground for creating *static site* built with the same technologies that I would use for a full-fledged Django app:
-
-* htmx
-* hyperscript
-* tailwind
-
 ## Note
-
-Create tailwind css files:
-
-```bash
-npx tailwindcss -i ./input.css -o ./static/output.css --watch
-```
 
 Python version 3.12
 
--- static/js/script.js
+++ static/js/script.js
@@ -2,4 +2,19 @@
 
 document.addEventListener("DOMContentLoaded", function async() {
   console.log("DOM fully loaded and parsed");
+  loadWords().then(() => {
+    console.log("Words loaded successfully");
+  }
+  ).catch((error) => {
+    console.error("Error loading words:", error);
+  });
 });
+
+
+const loadWords = async () => {
+  const response = await fetch("/static/assets/1000-words.txt");
+  const data = await response.text();
+  console.log(data);
+  const words = data.split("\n").map(word => word.trim()).filter(word => word.length > 0);
+  console.log(words);
+}
